68d0c1a30bb7f42dfd581ba1fef2dd2a7ff941da
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2022-05-05 20:07:33 +0900

    add

--- backend/webapp/aiapps/admin.py
+++ backend/webapp/aiapps/admin.py
@@ -3,8 +3,3 @@ from django.contrib import admin
 from .models import Comments, Images, Threads, UserToken, User
 
 # Register your models here.
-
-admin.site.register(Images)
-admin.site.register(Threads)
-admin.site.register(Comments)
-admin.site.register(User)
