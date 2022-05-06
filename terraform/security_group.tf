08f2c9bae1e511f3227c09e9501c27a31c66a9d5
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2022-05-07 01:04:40 +0900

    icm

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -11,14 +11,14 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-resource "aws_security_group_rule" "web_in_icmp" {
-  security_group_id = aws_security_group.web_sg.id
-  type              = "ingress"
-  protocol          = "icmp"
-  from_port         = -1
-  to_port           = -1
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+# resource "aws_security_group_rule" "web_in_icmp" {
+#   security_group_id = aws_security_group.web_sg.id
+#   type              = "ingress"
+#   protocol          = "icmp"
+#   from_port         = -1
+#   to_port           = -1
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
 
 resource "aws_security_group_rule" "web_in_http" {
   security_group_id = aws_security_group.web_sg.id
